e91ff66110144ad3b1baa2bb370f23eff3673bbe
Chinyere offor <obiagelichinyere566@gmail.com>
2024-03-17 22:44:58 -0400

    server up and running

---
+++ Server/controllers/route_functions.js
@@ -0,0 +1,255 @@
+import users_sequelize from "../models/users_sequelize.js";
+import listings_sequelize from "../models/listings_sequelize.js";
+
+
+const getUsers = async (req, res) => {
+  try {
+    const results = await users_sequelize.findAll();
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+//get many users
+const getUsersByConstraint = async (req, res) => {
+  try {
+    const constraint = req.params.constraint;
+    const value = req.params.value;
+    const results = await users_sequelize.findAllByConstraint(
+      constraint,
+      parseInt(value)
+    );
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//get one user
+const getUserByConstraint = async (req, res) => {
+  try {
+    const constraint = req.params.constraint;
+    const value = req.params.value;
+    //console.log(constraint, value);
+    const results = await users_sequelize.findOne(constraint, parseInt(value));
+    console.log(results.rows);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// create new user
+const createUser = async (req, res) => {
+  try {
+    const {
+        Name, 
+        Upcoming_trips, 
+        Past_trips, 
+        Wishlists, 
+        Address, 
+        Language, 
+        Intro, 
+        Ratings, 
+        Work, 
+        Picture_url,
+    } = req.body;
+    const results = await users_sequelize.create(
+        Name, 
+        Upcoming_trips, 
+        Past_trips, 
+        Wishlists, 
+        Address, 
+        Language, 
+        Intro, 
+        Ratings, 
+        Work, 
+        Picture_url,
+    );
+    res.status(201).json(results.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// update existing user
+const updateUser = async (req, res) => {
+  //console.log(req.body);
+  try {
+    const {
+        Name, 
+        Upcoming_trips, 
+        Past_trips, 
+        Wishlists, 
+        Address, 
+        Language, 
+        Intro, 
+        Ratings, 
+        Work, 
+        Picture_url,
+    } = req.body;
+    const results = await users_sequelize.update(
+        req.params.id,
+        Name, 
+        Upcoming_trips, 
+        Past_trips, 
+        Wishlists, 
+        Address, 
+        Language, 
+        Intro, 
+        Ratings, 
+        Work, 
+        Picture_url,
+    );
+    //console.log(results);
+    res.status(201).json(results.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete existing user
+const deleteUser = async (req, res) => {
+  try {
+    const { constraint, value } = req.body;
+    const results = await users_sequelize.deleteUser(constraint, value);
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+//now for listings
+const getListings = async (req, res) => {
+    try {
+      const results = await listings_sequelize.findAll();
+      res.status(200).json(results.rows);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  //get many listings
+  const getListingsByConstraint = async (req, res) => {
+    try {
+      const constraint = req.params.constraint;
+      const value = req.params.value;
+      const results = await listings_sequelize.findAllByConstraint(
+        constraint,
+        parseInt(value)
+      );
+      res.status(200).json(results.rows);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+  //get one listing
+  const getListingByConstraint = async (req, res) => {
+    try {
+      const constraint = req.params.constraint;
+      const value = req.params.value;
+      //console.log(constraint, value);
+      const results = await listings_sequelize.findOne(constraint, parseInt(value));
+      console.log(results.rows);
+      res.status(200).json(results.rows[0]);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+  // create new listing
+  const createListing = async (req, res) => {
+    try {
+      const {
+        Name, 
+        Location, 
+        Price_per_night, 
+        Type, 
+        Reviews, 
+        Host, 
+        About, 
+        Bedrooms, 
+        Amenities, 
+        Booked_days, 
+        Photo_gallery,
+      } = req.body;
+      const results = await listings_sequelize.create(
+        Name, 
+        Location, 
+        Price_per_night, 
+        Type, 
+        Reviews, 
+        Host, 
+        About, 
+        Bedrooms, 
+        Amenities, 
+        Booked_days, 
+        Photo_gallery,
+      );
+      res.status(201).json(results.rows);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+  // update existing listing
+  const updateListing = async (req, res) => {
+    //console.log(req.body);
+    try {
+      const {
+        Name, 
+        Location, 
+        Price_per_night, 
+        Type, 
+        Reviews, 
+        Host, 
+        About, 
+        Bedrooms, 
+        Amenities, 
+        Booked_days, 
+        Photo_gallery,
+      } = req.body;
+      const results = await listings_sequelize.update(
+        req.params.id,
+        Name, 
+        Location, 
+        Price_per_night, 
+        Type, 
+        Reviews, 
+        Host, 
+        About, 
+        Bedrooms, 
+        Amenities, 
+        Booked_days, 
+        Photo_gallery,
+      );
+      //console.log(results);
+      res.status(201).json(results.rows);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  
+  // delete existing user
+  const deleteListing = async (req, res) => {
+    try {
+      const { constraint, value } = req.body;
+      const results = await listings_sequelize.deleteUser(constraint, value);
+      res.status(200).json(results.rows);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+  export default {
+    getUsers,
+    getUsersByConstraint,
+    getUserByConstraint,
+    createUser,
+    updateUser,
+    deleteUser,
+    getListings,
+    getListingsByConstraint,
+    getListingByConstraint,
+    createListing,
+    updateListing,
+    deleteListing,
+    };
---
+++ Server/models/listings_sequelize.js
@@ -0,0 +1,47 @@
+import pool from '../config/database.js'
+
+const findAll = () => {
+  
+    const query = 'SELECT * FROM listings ORDER BY id ASC'
+    return pool.query(query)
+  
+}
+
+const findAllByConstraint = (constraint, value) => {
+  if (value) {
+    const query = `SELECT * FROM listings WHERE ${constraint}= $1 ORDER BY id ASC`
+    //console.log(pool.query(query, [constraint, value]))
+    return pool.query(query, [ value])
+  }
+  
+}
+
+const findOne = (constraint, value) => {
+  
+  const query = `SELECT * FROM listings WHERE ${constraint}= $1`
+  return pool.query(query, [value])
+}
+
+const create = (Name, Location, Price_per_night, Type, Reviews, Host, About, Bedrooms, Amenities, Booked_days, Photo_gallery) => {
+  
+    const query = 'INSERT INTO listings ( Name, Location, Price_per_night, Type, Reviews, Host, About, Bedrooms, Amenities, Booked_days, Photo_gallery) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *'
+    return pool.query(query, [ Name, Location, Price_per_night, Type, Reviews, Host, About, Bedrooms, Amenities, Booked_days, Photo_gallery])
+  }
+  
+const update = (id, Name, Location, Price_per_night, Type, Reviews, Host, About, Bedrooms, Amenities, Booked_days, Photo_gallery) => {
+    const query = 'UPDATE listings SET Name = $2, Location = $3, Price_per_night = $4, Type = $5, Reviews = $6, Host = $7, About=$8, Bedrooms=$9, Amenities=$10, Booked_days=$11, Photo_gallery=$12  WHERE id = $1'
+    return pool.query(query, [id, Name, Location, Price_per_night, Type, Reviews, Host, About, Bedrooms, Amenities, Booked_days, Photo_gallery])
+  }
+  
+const deleteListing = (constraint,value) => {
+    const query = `DELETE FROM listings WHERE ${constraint} = $1`
+    return pool.query(query, [value])
+  }
+export default {
+  findAll,
+  findAllByConstraint,
+  findOne,
+  create,
+  update,
+  deleteListing
+}
