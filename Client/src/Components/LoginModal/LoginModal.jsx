63236515231088a12e8af24639ea0421b235aff6
Chinyere offor <obiagelichinyere566@gmail.com>
2024-03-19 20:08:10 -0400

    login working

---
+++ Client/src/Components/LoginModal/LoginModal.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router-dom";
+//import { useContext } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState, useContext } from "react";
+import Form from "react-bootstrap/Form";
+import { UserContext } from "../../UserContext";
+
+import "./LoginModal.css";
+
+function MyVerticallyCenteredModal(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { userContext, setUserContext } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    //e.preventDefault();
+    console.log("here");
+
+    try {
+      console.log("inner");
+      // Make the login API request
+      //const url = `${apiUrlContext}/job/${"id"}/${jobId}`;
+      const url = `http://localhost:3000/login/${username}/${password}`;
+
+      const response = await fetch(url);
+      console.log(response);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+
+        //const loggedInUser = data.user;
+
+        // Update the user context
+        setUserContext(data);
+
+        // Navigate to the home page after successful login
+        navigate("/");
+      } else {
+        // Handle the login failure case
+        alert("Login failed");
+      }
+    } catch (error) {
+      // Handle any network or API request errors
+      alert("Login failed: " + error);
+    }
+  };
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              required
+              type="username"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button
+            onClick={() => handleLogin()}
+            style={{ backgroundColor: "rgba(227, 80, 124)" }}
+          >
+            Login
+          </Button>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-secondary" onClick={props.onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default function LoginModal() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button
+        style={{
+          backgroundColor: "rgba(227, 80, 124)",
+          width: "200px",
+        }}
+        onClick={() => setModalShow(true)}
+      >
+        Login/Signup
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
--- Server/config/routes.js
+++ Server/config/routes.js
@@ -3,6 +3,7 @@ import express from "express";
 import route_functions from "../controllers/route_functions.js";
 
 const router = express.Router();
+router.get("/login/:username/:password", route_functions.LoginUser);
 router.get("/users", route_functions.getUsers);
 router.get("/users/:constraint/:value", route_functions.getUsersByConstraint);
 router.get("/user/:constraint/:value", route_functions.getUserByConstraint);
@@ -12,8 +13,14 @@ router.patch("/user/:id", route_functions.updateUser);
 router.delete("/car", route_functions.deleteUser);
 //everything company
 router.get("/listings", route_functions.getListings);
-router.get("/listings/:constraint/:value", route_functions.getListingsByConstraint);
-router.get("/listing/:constraint/:value",  route_functions.getListingByConstraint);
+router.get(
+  "/listings/:constraint/:value",
+  route_functions.getListingsByConstraint
+);
+router.get(
+  "/listing/:constraint/:value",
+  route_functions.getListingByConstraint
+);
 router.post("/listing", route_functions.createListing);
 router.patch("/listing/:id", route_functions.updateListing);
 router.delete("/listing", route_functions.deleteListing);
