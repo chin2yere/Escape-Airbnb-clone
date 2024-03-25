46bfbe1f0fd11b3c2fd031314386ac2ed142e515
Chinyere offor <obiagelichinyere566@gmail.com>
2024-03-25 02:33:15 -0400

    Category functionality ongoing

--- Client/src/Components/HomeCategoryBar/HomeCategoryBar.jsx
+++ Client/src/Components/HomeCategoryBar/HomeCategoryBar.jsx
@@ -7,43 +7,145 @@ import { MdCottage } from "react-icons/md";
 import { GiWoodCabin } from "react-icons/gi";
 import { MdCabin } from "react-icons/md";
 import { GiFamilyHouse } from "react-icons/gi"; //condo
-
-export default function HomeCategoryBar() {
+import { BsCheckAll } from "react-icons/bs";
+import HomeFilterModal from "../HomeFilterModal/HomeFilterModal";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+export default function HomeCategoryBar({ category, setCategory }) {
+  function setAll() {
+    const temp = {
+      Apartment: true,
+      Villa: true,
+      Loft: true,
+      Condo: true,
+      Cottage: true,
+      Cabin: true,
+    };
+    setCategory({ ...temp });
+  }
+  function setApartment() {
+    const temp = {
+      Apartment: true,
+      Villa: false,
+      Loft: false,
+      Condo: false,
+      Cottage: false,
+      Cabin: false,
+    };
+    setCategory({ ...temp });
+  }
+  function setVilla() {
+    const temp = {
+      Apartment: false,
+      Villa: true,
+      Loft: false,
+      Condo: false,
+      Cottage: false,
+      Cabin: false,
+    };
+    setCategory({ ...temp });
+  }
+  function setLoft() {
+    const temp = {
+      Apartment: false,
+      Villa: false,
+      Loft: true,
+      Condo: false,
+      Cottage: false,
+      Cabin: false,
+    };
+    setCategory({ ...temp });
+  }
+  function setCondo() {
+    const temp = {
+      Apartment: false,
+      Villa: false,
+      Loft: false,
+      Condo: true,
+      Cottage: false,
+      Cabin: false,
+    };
+    setCategory({ ...temp });
+  }
+  function setCottage() {
+    const temp = {
+      Apartment: false,
+      Villa: false,
+      Loft: false,
+      Condo: false,
+      Cottage: true,
+      Cabin: false,
+    };
+    setCategory({ ...temp });
+  }
+  function setCabin() {
+    const temp = {
+      Apartment: false,
+      Villa: false,
+      Loft: false,
+      Condo: false,
+      Cottage: false,
+      Cabin: true,
+    };
+    setCategory({ ...temp });
+  }
+  console.log(category);
   return (
     <div className="home-category-bar">
-      <Nav variant="underline">
-        <Nav.Item style={{ marginRight: "20px" }}>
-          <Nav.Link>
-            <MdApartment style={{ fontSize: "34px" }} />
-            <h5>hiii</h5>
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item style={{ marginRight: "20px" }}>
-          <Nav.Link>
-            <MdVilla style={{ fontSize: "34px" }} />
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item style={{ marginRight: "20px" }}>
-          <Nav.Link>
-            <MdCottage style={{ fontSize: "34px" }} />
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item style={{ marginRight: "20px" }}>
-          <Nav.Link>
-            <GiFamilyHouse style={{ fontSize: "34px" }} />
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item style={{ marginRight: "20px" }}>
-          <Nav.Link>
-            <GiWoodCabin style={{ fontSize: "34px" }} />
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item style={{ marginRight: "20px" }}>
-          <Nav.Link>
-            <MdCabin style={{ fontSize: "34px" }} />
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Row>
+        <Col sm={10}>
+          <Nav variant="underline">
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link style={{ color: "black" }} onClick={() => setAll()}>
+                <BsCheckAll style={{ fontSize: "34px" }} />
+                <p>All types</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link
+                style={{ color: "black" }}
+                onClick={() => setApartment()}
+              >
+                <MdApartment style={{ fontSize: "34px" }} />
+                <p>Apartment</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link style={{ color: "black" }} onClick={() => setVilla()}>
+                <MdVilla style={{ fontSize: "34px" }} />
+                <p>Villa</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link style={{ color: "black" }} onClick={() => setCottage()}>
+                <MdCottage style={{ fontSize: "34px" }} />
+                <p>Cottage</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link style={{ color: "black" }} onClick={() => setCondo()}>
+                <GiFamilyHouse style={{ fontSize: "34px" }} />
+                <p>Condo</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link style={{ color: "black" }} onClick={() => setLoft()}>
+                <GiWoodCabin style={{ fontSize: "34px" }} />
+                <p>Loft</p>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item style={{ marginRight: "20px" }}>
+              <Nav.Link style={{ color: "black" }} onClick={() => setCabin()}>
+                <MdCabin style={{ fontSize: "34px" }} />
+                <p>Cabin</p>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={2}>
+          <HomeFilterModal />
+        </Col>
+      </Row>
     </div>
   );
 }
--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import HomeTopBar from "../../Components/HomeTopBar/HomeTopBar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,17 +9,30 @@ import HomeCategoryBar from "../../Components/HomeCategoryBar/HomeCategoryBar";
 import "./Home.css";
 
 export default function Home() {
+  const [category, setCategory] = useState(() => {
+    const temp = {
+      Apartment: true,
+      Villa: true,
+      Loft: true,
+      Condo: true,
+      Cottage: true,
+      Cabin: true,
+    };
+    return temp;
+  });
+  //console.log(category);
   return (
     <div className="home">
       <Container fluid>
         <Row className="top-bar">
           <HomeTopBar />
-          <HomeCategoryBar />
+          <HomeCategoryBar category={category} setCategory={setCategory} />
         </Row>
+
         {/* <Row style={{ marginTop: "150px" }}>
           
         </Row> */}
-        <Row style={{ marginTop: "150px" }}>
+        <Row style={{ marginTop: "260px" }}>
           <Grid />
         </Row>
       </Container>
